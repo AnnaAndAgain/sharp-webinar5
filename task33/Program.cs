4044b90805125ade1a5e65d1404d22e4cce1e7b3
Anna <a.s.markovskaya@gmail.com>
2023-07-08 14:43:51 +0300

    + task 2

---
+++ task33/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Меняем в массиве все эелементы на противоположные 
+по знаку (пол на отр, отр на пол)
+*/ 
+
+int[] FillArrayRandom(int tmpLength, int tmpMin, int tmpMax)
+{
+    int[] tmpArray = new int[tmpLength];
+    Random randnum = new Random();
+
+    for (int i = 0; i < tmpArray.Length; i++)
+    {
+        tmpArray[i] = randnum.Next(tmpMin, tmpMax + 1);
+    };
+
+    return tmpArray;
+}
+
+void PrintArray(int[] tmpArray)
+{
+    Console.WriteLine($"[" + string.Join(", ", tmpArray) + "]");
+}
+
+void ChangeSign(int[] tmpArray)
+{
+    for (int i =0; i < tmpArray.Length; i++)
+    {
+        tmpArray[i]=-1*tmpArray[i];
+    }
+}
+
+int[] myArray = FillArrayRandom(10, -100, 100);
+PrintArray(myArray);
+ChangeSign(myArray);
+PrintArray(myArray);
